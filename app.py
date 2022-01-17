4428e95b5817042301006a88d67eee8b363a777b
bhavikjadav <bhavik.jadav89@gmail.com>
2022-01-17 06:50:50 +0530

    dashboard frontend working

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from email import header
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask import Flask, render_template, redirect, request, session, flash
 import sqlite3
 import pandas as pd
 
@@ -16,11 +16,11 @@ app.secret_key = 'MachineLearningisLife#123'
 def login():
     if(request.method == 'POST'):
         username = request.form.get('username')
-        password = request.form.get('password')     
+        password = request.form.get('password')
         if username == admin_auth_data.iat[0, 0] and password == admin_auth_data.iat[0, 1]: 
-            session['user'] = username 
+            session['user'] = username
             return redirect('/dashboard')
-        flash("Wrong Username or Password")   
+        flash("Wrong Username or Password")
         return redirect('/')
     return render_template("login.html")
 
@@ -29,7 +29,7 @@ def dashboard():
     if('user' in session and session['user'] == admin_auth_data.iat[0, 0]):
         return render_template('dashboard.html')
     #here we are checking whether the user is logged in or not
-
     return redirect('/')  #if the user is not in the session
+
 if __name__ == '__main__':
     app.run(debug=True)
